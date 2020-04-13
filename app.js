1a8143ea9ba3e45156571e99a762404db47f7b4b
Raja Rupinder Singh <rajarupinder@gmail.com>
2020-04-13 15:42:39 +0530

    Reading Inputs

--- app.js
+++ app.js
@@ -4,14 +4,40 @@ var budgetController = (function () {
 })();
 
 var UIController = (function () {
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+        return {
+                getInput: function () {
+                    return {
+                        type: document.querySelector(DOMStrings.inputType).value, //in for income and exp for expense
+                        description: document.querySelector(DOMStrings.inputDescription).value,
+                        value: document.querySelector(DOMStrings.inputValue).value
+                        
+                    };
+            
+                },
+                getDOMstrings: function () {
+                return DOMStrings;
+                }
+        };
     
 })();
 
 var controller = (function (budgetCtrl,UICtrl) {
     var ctrlAddItem = function () {
-        console.log('it works.');
+        var input = UICtrl.getInput();
+        console.log(input);
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-    document.addEventListener('keypress', ctrlAddItem);
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
     
 })(budgetController,UIController);
